fcafb287cdbf500b49916c41ac3a299c5974bde0
jorge <jorge mauro goncalves>
2018-08-30 17:05:34 -0300

    ionic config

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {NavController, NavParams} from 'ionic-angular';
+import {TipoRequisito} from "../../modelo/TipoRequisito";
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  public tiporequisito:TipoRequisito[];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _http:HttpClient) {
+    // this._http.get<TipoRequisito[]>('http://camerascasas.no-ip.info:8085/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/null')
+    //   .subscribe(
+    //     (tiporequisito)=>{
+    //       console.log(tiporequisito);
+    //       this.tiporequisito=tiporequisito;
+    //     }
+    //   );
+    this.tiporequisito=[{codtipreq:1,nomtipreq:'requisito 1' },{codtipreq:2, nomtipreq:'requisito2'}];
   }
 
 }
