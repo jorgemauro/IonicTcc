105ae2718723fc57a58bd97d452049d1058fc63b
jorge <jorge mauro goncalves>
2018-09-10 16:00:14 -0300

    Aleluia PROXY!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,14 +10,14 @@ export class HomePage {
 
   public tiporequisito:TipoRequisito[];
   constructor(public navCtrl: NavController, public navParams: NavParams, private _http:HttpClient) {
-    // this._http.get<TipoRequisito[]>('http://camerascasas.no-ip.info:8085/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/null')
-    //   .subscribe(
-    //     (tiporequisito)=>{
-    //       console.log(tiporequisito);
-    //       this.tiporequisito=tiporequisito;
-    //     }
-    //   );
-    this.tiporequisito=[{codtipreq:1,nomtipreq:'requisito 1' },{codtipreq:2, nomtipreq:'requisito2'}];
+    this._http.get<TipoRequisito[]>('/api/GenericRestService/rest/querytojson/LISTTIPOREQUISITO/null')
+      .subscribe(
+        (tiporequisito)=>{
+          console.log(tiporequisito);
+          this.tiporequisito=tiporequisito;
+        }
+      );
+  //  this.tiporequisito=[{codtipreq:1,nomtipreq:'requisito 1' },{codtipreq:2, nomtipreq:'requisito2'}];
   }
 
 }
